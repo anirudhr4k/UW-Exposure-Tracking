451a32e62c53ed85c511fa1db49d9eff06d12e4c
Anirudh <24980391+DarkLink77689@users.noreply.github.com>
2021-10-23 16:08:51 -0700

    add server

--- UWET/src/pages/Home.tsx
+++ UWET/src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>UW Exposure Tracking</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>

865d6b7ecbcb99211683260741250f967852e068
HrikBho <hrik.bhowal@gmail.com>
2021-10-23 15:54:27 -0700

    began mappage

--- UWET/src/App.js
+++ UWET/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import MapPage from "./components/MapPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MapPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ UWET/src/components/MapPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./MapPage.css";
+
+function MapPage() {
+  return (
+    <div className="map__page">
+      <button>
+        <p>Log Out</p>
+      </button>
+
+      {/*Search Bar*/}
+      {/*Map*/}
+      {/*My Classes Section*/}
+    </div>
+  );
+}
+
+export default MapPage;

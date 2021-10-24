4e50ca53f9041222a85ca7056f9a458a0312bac2
Anirudh <24980391+DarkLink77689@users.noreply.github.com>
2021-10-24 02:19:00 -0700

    more progress

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,40 +1,79 @@
 import { IonModal, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonFooter, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonTitle, IonToolbar, IonSearchbar, IonList, IonItem, IonLabel, IonChip, IonInput, IonSelect, IonSelectOption } from '@ionic/react';
 import axios from 'axios'
+import GoogleMapReact from 'google-map-react'
+
+import MyMarker from './MyMarker';
+
 import { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
-import { alert, link } from 'ionicons/icons';
+import { add, alert, link } from 'ionicons/icons';
 import './Home.css';
 
 const Home: React.FC = () => {
 
   let exposureContainer: any;
 
+  const points = [
+    { id: 1, title: "Mary Gates Hall", lat: 47.65489345919378, lng: -122.30782943077774 },
+    { id: 2, title: "Paul Allen Center for CS", lat: 47.65340354952161, lng: -122.30590811728844 },
+    { id: 3, title: "Bagley Hall", lat: 47.652963773474845, lng: -122.30863304284266 }
+  ];
+
   const [showModal, setShowModal] = useState(false);
   const [searchModal, setSearchModal] = useState(false);
   const [exposureModal, setExposureModal] = useState(false);
   const [addClassModal, setAddClassModal] = useState(false);
   const [searchText, setSearchText] = useState("");
+  const [roomNum, setRoomNum] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const [classes, setClasses] = useState<any[]>([]);
+
+  const [addBldg, setAddBldg] = useState("");
+  const [addRmNum, setAddRmNum] = useState("");
 
   const [cbldg, setCbldg] = useState("");
   const [crm, setCrm] = useState("");
   const [cnum_cases, setCnum_cases] = useState("");
   const [clocs, setClocs] = useState("[]");
 
+  const [showMapModal, setShowMapModal] = useState(false);
+
   const [exposureRoomPart, setExposureRoomPart] = useState("5")
 
 
-  async function getBldgs(name: string) {
+  async function getBldgs(name: string, rm: string) {
     try {
-      let response = await axios.get(`http://172.22.1.3:8000/api/buildings/get-buildings/${name}`)
-      console.log(response)
+      if (name == "") {
+        let default_val: any = [(<IonItem><IonLabel>Try searching for a building at UW!</IonLabel></IonItem>)]
+        setSearchResults(default_val)
+      } else {
+        let response: any = await axios.get(`http://localhost:8000/api/buildings/get-buildings/${name}`)
+        let data = response.data
+        let lists: any = []
+        if (data.length == 0) {
+          lists.push((<IonItem><IonLabel>Uh oh.. We found no building by that name</IonLabel></IonItem>))
+        } else {
+          for (let i of data) {
+            lists.push((<IonItem onClick={() => showModalWithData(i.name, rm)}><IonLabel>{i.name}</IonLabel></IonItem>))
+          }
+        }
+        setSearchResults(lists)
+
+      }
     } catch (err: any) {
       console.log(err.response)
     }
   }
 
   function handleSearching(x: any) {
-    getBldgs(x)
     setSearchText(x)
+    getBldgs(x, roomNum)
+  }
+
+  function handleRoomNum(x: any) {
+    setRoomNum(x)
+    getBldgs(searchText, x)
   }
 
   function editExposureContainer(tbldg: string, trm: string) {
@@ -52,7 +91,6 @@ const Home: React.FC = () => {
   }
 
   function getClasses() {
-    let classes: any[] = [{ bldg: "gug", rm: "220" }, { bldg: "ece", rm: "045" }]
 
     if (classes.length == 0) {
       return ["Uh oh.. you don't have any classes yet.. add some!"]
@@ -84,11 +122,13 @@ const Home: React.FC = () => {
             <IonTitle size="large">UW Exposure</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonButton expand="block" size="small" onClick={() => setSearchModal(true)} fill="outline">Search for buildings...</IonButton>
+        <IonButton expand="block" size="small" onClick={() => setSearchModal(true)} fill="outline">Search for rooms by building</IonButton>
+
         <IonCard>
           <IonCardHeader>
             <IonCardTitle>Your classes </IonCardTitle>
             <IonButton size="small" color="success" onClick={() => setAddClassModal(true)}>Add a new class</IonButton>
+            <IonButton size="small" color="success" onClick={() => setShowMapModal(true)}>Show map of area</IonButton>
             {/*<IonButton size="small" onClick={() => setShowModal(true)}>Test Button To Open Class Modal</IonButton>*/}
           </IonCardHeader>
           <IonCardContent>
@@ -125,6 +165,12 @@ const Home: React.FC = () => {
 
       <IonModal isOpen={searchModal}>
         <IonSearchbar value={searchText} placeholder="Search for a building here..." onIonChange={e => handleSearching(e.detail.value!)}></IonSearchbar>
+        <IonSearchbar value={roomNum} placeholder="Enter room number here..." onIonChange={e => handleRoomNum(e.detail.value!)}></IonSearchbar>
+        <IonContent>
+          <IonList>
+            {searchResults}
+          </IonList>
+        </IonContent>
         <IonButton onClick={() => setSearchModal(false)}>Back</IonButton>
       </IonModal>
 
@@ -137,24 +183,46 @@ const Home: React.FC = () => {
         <IonContent>
           <IonItem>
             <IonLabel position="stacked">Building</IonLabel>
-            <IonInput> </IonInput>
+            <IonInput value={addBldg} onIonChange={(e) => setAddBldg(e.detail.value!)}> </IonInput>
           </IonItem>
           <IonItem>
             <IonLabel position="stacked">Room #</IonLabel>
-            <IonInput> </IonInput>
+            <IonInput value={addRmNum} onIonChange={(e) => setAddRmNum(e.detail.value!)}> </IonInput>
           </IonItem>
         </IonContent>
         <IonFooter>
-          <IonButton expand="block" onClick={() => setAddClassModal(false)} color="success">Add</IonButton>
+          <IonButton expand="block" onClick={() => setAddClassModal(false)} color="success" disabled={(addBldg == "" || addRmNum == "") ? true : false}>Add</IonButton>
           <br />
           <IonButton expand="block" onClick={() => setAddClassModal(false)} color="danger">Cancel</IonButton>
+        </IonFooter>
+      </IonModal>
+
+      <IonModal isOpen={showMapModal}>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: "AIzaSyBqEpiQkFa5ZBlRRLJCXeIlii9vddl2pmY",
+            libraries: ['visualization'],
+            language: "en",
+            region: "US"
+          }}
+          defaultCenter={{ lat: 47.65471269808691, lng: -122.30845939266062 }}
+          defaultZoom={16}
+        >
+          {points.map(({ lat, lng, id, title }) => {
+            return (
+              MyMarker
+            );
+          })}
+        </GoogleMapReact>
+        <IonFooter>
+          <IonButton expand="block" onClick={() => setShowMapModal(false)} color="danger">Back</IonButton>
         </IonFooter>
       </IonModal>
 
       <IonModal isOpen={exposureModal}>
         <IonHeader>
           <IonToolbar>
-            <IonTitle>Where did you go in the last days? (Note: you will have to fill this out for each room)</IonTitle>
+            <IonTitle size="small">Where did you go in the last days? (Note: you will have to fill this out for each room)</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent>
